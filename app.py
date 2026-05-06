17cdd3cad86cf263dddb19998d80cd4cadc0560b
pm2k04 <157699681+pm2k04@users.noreply.github.com>
2026-05-06 10:49:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ with open("Salary_model.pkl", "rb") as f:
 
 @app.get("/")
 def home():
-    return {"message": "dhrumil joshi"}
+    return {"message": "prince patel hello  how are you good morning"}
 
 @app.post("/predict")
 def predict(data: dict):
